3c29a16ecf94443dad1c7899e54ff358b7af934a
alex <bez-al@mail.ru>
2022-02-01 23:07:57 +0300

    last

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 let express = require('express');
-let Client = require('stratum').Client;
+
 let bodyParser = require('body-parser');
 let app = express();
 app.use(express.static('dist'));
@@ -7,25 +7,4 @@ app.listen(8080, ()=>{
 	console.log('Server run 8080 port');
 	
 });
-var client = new stratum.Client();
 
-app.use(bodyParser.json());
-app.post('/check_stratum_status', (reg, res,)=>{
-	var adress = reg.body;
-	console.log('stratum', adress)
-	client = Client.create();
-	client.connect({
-		host: adress.stratum,
-		port: adress.coin
-	}).then(function(a){ return a
-	}).then(function(value){
-		currStat={
-			name:adress.stratum,
-				status: 'green'
-			}
-		console.log(currStat)
-		res.send(currStat);
-		
-	});
-
-}) 
